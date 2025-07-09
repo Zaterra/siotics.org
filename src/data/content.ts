4495d723dbe863b6c2f658c93547363fff3cbc4f
mraihanaf <mraihanaf0@gmail.com>
2025-07-09 18:50:52 +0700

    feat: add data/content.ts

---
+++ src/data/content.ts
@@ -0,0 +1,33 @@
+import {
+    Network,
+    Cpu,
+    Zap,
+    Lightbulb,
+    Trophy
+} from "lucide-react"
+
+export const ActivitiesData = [
+    {icon: Lightbulb, title: "Innovates", content: "Innovate from what you learned to solves real problem."},
+    {icon: Trophy, title: "Competitions", content: "Join exciting competitions and participate together with your club friends as a team."},
+    {icon: Cpu, title: "Robotics Projecs", content: "Design and build autonomous robots."},
+    {icon: Zap, title: "Coding Sessions", content: "Start with programming fundamentals and then learn languages like C++, TypeScript, JavaScript, and Python."},
+    {icon: Network, title: "IoT Workshops", content: "Hands-on sessions learning to connect sensors, build smart devices, monitoring systems, and interactive projects with Internet of Things technology."}
+]
+
+export const FAQsData = [
+    {question: "Who is allowed to join?", answer: "Siotics is open for all State Vocational 1 Jakarta's students"},
+    {question: "Is this extracurricular only for informatics student?", answer: "No, this extracurricular is open to all students from State Vocational 1 Jakarta's students"},
+    {question: "Do i need a laptop to join this extracurricular?", answer: "Having a laptop is helpful, but it's not mandatory. We provide shared devices for practice if needed."},
+    {question: "When and where are the meetings held?", answer: "Meetings are held every Tuesday at 3 PM in the Informatics Building, Computer Lab, Room 405. Updates will be shared in our group chat."},
+    {question: "Do i need prior knowledge about IoT and Robotics?", answer: "Not at all! We welcome beginners and will guide you from the basics through workshops and projects."},
+    {question: "What skills can i learn in this extracurricular?", answer: "You can learn coding, electronics, teamwork, problem-solving, and how to build real IoT and robotics projects."}
+]
+
+export const teamData = [
+    {name: "Muhammad Raihan Al Farizy", profilePicturePath: "raihan.webp"},
+    {name: "Kim Minji", profilePicturePath: "minji.webp"},
+    {name: "Muhammad Raihan Al Farizy", profilePicturePath: "raihan.webp"},
+    {name: "Kim Minji", profilePicturePath: "minji.webp"},
+    {name: "Muhammad Raihan Al Farizy", profilePicturePath: "raihan.webp"},
+    {name: "Kim Minji", profilePicturePath: "minji.webp"},
+]
