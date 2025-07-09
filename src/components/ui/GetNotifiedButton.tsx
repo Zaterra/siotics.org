afae7686331017fa5bf1275f998c28aeb824a7c5
mraihanaf <mraihanaf0@gmail.com>
2025-07-09 19:23:39 +0700

    feat: make get notification button work

--- src/components/ui/GetNotifiedButton.tsx
+++ src/components/ui/GetNotifiedButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 import { Calendar, ArrowRight } from "lucide-react"
 import { cn } from "@/lib/utils"
 
@@ -7,10 +8,13 @@ export default function GetNotifiedButton({ className = "" }: { className?: stri
             <Button
               size="sm"
               className={cn("bg-blue-600 hover:bg-blue-700 text-white group shadow-lg shadow-blue-600/25 ", className)}
+              asChild
             >
+              <Link target="_blank" href={"https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20250718%2F20250719&details=Siotics%20Registration%20is%20open%21%20Checkout%20https%3A%2F%2Fsiotics.org&location=&text=Siotics%20Registration"}>
               <Calendar className="mr-2 h-4 w-4" />
               Get Notified
               <ArrowRight className="ml-2 h-4 w-4 group-hover:translate-x-1 transition-transform" />
+              </Link>
             </Button>
     )
 }
